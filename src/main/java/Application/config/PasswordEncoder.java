42502d8b82b270dcfaaff9e685412d1eeef15037
Nikolai Rodionov <nikolua-r@mail.ru>
2021-09-15 10:25:05 +0300

    Add_Twitter_Initial

---
+++ src/main/java/Application/config/PasswordEncoder.java
@@ -0,0 +1,12 @@
+package Application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+@Configuration
+public class PasswordEncoder {
+    @Bean
+    public org.springframework.security.crypto.password.PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder(8);
+    }
+}
